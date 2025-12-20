bd1161b628dcd9cc0fc6ad44a0f043fe20bdf7d9
Aakash <aakashjangrawork77@gmail.com>
2025-12-21 00:57:33 +0530

    Add github actions Ci Pipline

--- src/test/java/com/sdet/framework/driver/DriverFactory.java
+++ src/test/java/com/sdet/framework/driver/DriverFactory.java
@@ -1,10 +1,11 @@
 package com.sdet.framework.driver;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import com.sdet.framework.utils.ConfigReader;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
@@ -13,11 +14,25 @@ public class DriverFactory {
 
     public static void initDriver() {
         if (driver.get() == null) {
+
             String browser = ConfigReader.getProperty("browser");
 
             if (browser.equalsIgnoreCase("chrome")) {
+
                 WebDriverManager.chromedriver().setup();
-                driver.set(new ChromeDriver());
+
+                ChromeOptions options = new ChromeOptions();
+
+                //Headless support
+                if (System.getProperty("headless", "false")
+                        .equalsIgnoreCase("true")) {
+
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                }
+
+                driver.set(new ChromeDriver(options));
             }
 
             driver.get().manage().window().maximize();
